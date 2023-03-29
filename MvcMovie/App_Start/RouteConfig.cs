37b3fd14388cd47729a75eaefa86b0c3088a0ade
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-29 10:24:55 -0600

    el archivo RoutConfig se modifico al igual que los metodos de en el controlodar HelloWorldController (helloworld)

--- MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -20,6 +20,12 @@ namespace MvcMovie
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
+
         }
     }
 }
--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -16,12 +16,18 @@ namespace MvcMovie.Controllers
             return "this is my <b> default <b/> action";
         }
 
+        public string Welcome(string name, int ID=1) {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        
+        }
+
+        /*
         public string Welcome(string name, int numTimes = 1 ) {
             return HttpUtility.HtmlEncode("Hello " + name + ", Numtimes is: "+ numTimes);
             //https://localhost:44398/HelloWorld/welcome?name=Scott&numtimes=4
             //el signo ?, en la direcion de URL es un separador y siguen las cadenas de consulta
             //nota de seguridad: el codigo anterior usa httpUtility.HtmlEncode para proteger el codigo de entradas
             //mal intencionadas (es decir JS)
-        }
+        }*/
     }
 }
