595f9da46c12ed8dca6c42919b7ebaa28e0ad2c1
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-29 10:08:18 -0600

    Agregar archivos de proyecto.

---
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                //cuando se ejecuta la aplicacion y no se proporcionan segmentos de direccion URL, el valor predeterminado
+                //es el controlador de inicio "home"
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+*/
+
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "this is my <b> default <b/> action";
+        }
+
+        public string Welcome(string name, int numTimes = 1 ) {
+            return HttpUtility.HtmlEncode("Hello " + name + ", Numtimes is: "+ numTimes);
+            //https://localhost:44398/HelloWorld/welcome?name=Scott&numtimes=4
+            //el signo ?, en la direcion de URL es un separador y siguen las cadenas de consulta
+            //nota de seguridad: el codigo anterior usa httpUtility.HtmlEncode para proteger el codigo de entradas
+            //mal intencionadas (es decir JS)
+        }
+    }
+}
