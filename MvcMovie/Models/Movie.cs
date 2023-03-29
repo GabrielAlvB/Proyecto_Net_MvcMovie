aea220c674e70c66b191a8c73885b7b3556a03c1
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-29 11:38:51 -0600

    Problemas con el paquete Nuget de entity framework

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//para hacer referencia a Dbcontext y a Dbset se requiere hacer uso de: 
+using System.Data.Entity;
+//faltara instalar algo? paquete Nuget de entity framework????
+
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext 
+    {
+    public DbSet<Movie> Movies { get; set; }
+    }
+
+}
