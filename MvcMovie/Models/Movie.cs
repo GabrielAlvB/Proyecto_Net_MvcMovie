4171780ffc1cdf6333e3a54c227912c08fbce93c
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-30 11:26:44 -0600

    a

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -4,6 +4,8 @@
 //using System.Web;
 
 //para hacer referencia a Dbcontext y a Dbset se requiere hacer uso de: 
+
+
 using System.Data.Entity;
 //faltara instalar algo? paquete Nuget de entity framework????
 
@@ -18,10 +20,12 @@ namespace MvcMovie.Models
         public string Genre { get; set; }
         public decimal Price { get; set; }
     }
-
+    
     public class MovieDBContext : DbContext 
     {
     public DbSet<Movie> Movies { get; set; }
     }
+    
+     
 
 }
